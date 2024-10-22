65c8620522ca6322d83a1481a2f408587c032475
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2024-10-22 12:03:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-blue-300 text-4xl'>knowledge cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
